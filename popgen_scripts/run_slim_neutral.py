6feda89a831b2194368c4f70b9871da03a20bc5e
Lauren Sugden <laurensugden@Eliza.local>
2020-07-16 14:14:59 -0400

    needs definition argument

--- popgen_scripts/run_slim_neutral.py
+++ popgen_scripts/run_slim_neutral.py
@@ -1,4 +1,5 @@
 from popgen_utils.haplotype_simulation import slim
-slim.run_slim_neutral(project_name='sweep_hmm',
-             model_name='gravel_neutral_100kb')
+slim.run_slim_neutral('gravel_model_neutral_100kb',
+					  project_name='sweep_hmm',
+            		  model_name='gravel_neutral_100kb')
 
