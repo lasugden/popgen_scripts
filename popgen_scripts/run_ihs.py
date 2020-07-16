02f53cd23892d70834b1ac667979142707c25a5d
Lauren Sugden <laurensugden@Eliza.local>
2020-07-16 17:52:08 -0400

    scripts to run selscan

---
+++ popgen_scripts/run_ihs.py
@@ -0,0 +1,6 @@
+from popgen_utils.snp_statistics import selscan
+selscan.run_ihs(project_name='sweep_hmm',
+             model_name='gravel_sweep_100kb_uniform_scoeff',type='sweep')
+
+selscan.run_ihs(project_name='sweep_hmm',
+             model_name='gravel_neutral_100kb',type='neutral')
