0fa0ffdcac16d60de5d5725a863fa7a43213e5d9
Lauren Sugden <laurensugden@Eliza.local>
2020-07-16 13:56:53 -0400

    run slim on neutral demography

---
+++ popgen_scripts/generate_slim_input_neutral.py
@@ -0,0 +1,6 @@
+from popgen_utils.haplotype_simulation import slim
+slim.slim_definition_to_input_neutral('gravel_model_neutral_100kb',
+                             		      project_name='sweep_hmm',
+                             		      model_name='gravel_neutral_100kb',
+                             		      sims=1000)
+
---
+++ popgen_scripts/run_slim_neutral.py
@@ -0,0 +1,4 @@
+from popgen_utils.haplotype_simulation import slim
+slim.run_slim_neutral(project_name='sweep_hmm',
+             model_name='gravel_neutral_100kb')
+
