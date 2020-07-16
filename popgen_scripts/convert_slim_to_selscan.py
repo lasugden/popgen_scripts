ebc85445ec5783b45ae0badf2c4e84435bcef978
Lauren Sugden <laurensugden@Eliza.local>
2020-07-16 15:06:03 -0400

    run tools from process_slim_output

---
+++ popgen_scripts/convert_slim_to_selscan.py
@@ -0,0 +1,6 @@
+from popgen_utils.haplotype_simulation import process_slim_output
+process_slim_output.slim_out_to_selscan(project_name='sweep_hmm',
+             model_name='gravel_sweep_100kb_uniform_scoeff',type='sweep')
+
+process_slim_output.slim_out_to_selscan(project_name='sweep_hmm',
+             model_name='gravel_neutral_100kb',type='neutral')
