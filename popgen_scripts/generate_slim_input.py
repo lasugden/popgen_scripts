43af4867d4f66801cd937adbc7ad8f9199496bc2
Lauren Sugden <laurensugden@Eliza.local>
2020-07-23 10:38:30 -0400

    run on 400kb

--- popgen_scripts/generate_slim_input.py
+++ popgen_scripts/generate_slim_input.py
@@ -1,9 +1,17 @@
 from popgen_utils.haplotype_simulation import slim
-slim.slim_definition_to_input('gravel_model_sweep_100kb',
+# slim.slim_definition_to_input('gravel_model_sweep_100kb',
+#                         		      selection_coefficient_min=0.02,
+#                              		      selection_coefficient_max=0.12,
+#                              		      sweep_population=['p1', 'p2', 'p3'],
+#                              		      sweep_time=[5,10,15,20],
+#                              		      project_name='sweep_hmm',
+#                              		      model_name='gravel_sweep_100kb_uniform_scoeff')
+
+slim.slim_definition_to_input('gravel_model_sweep_400kb',
                         		      selection_coefficient_min=0.02,
                              		      selection_coefficient_max=0.12,
                              		      sweep_population=['p1', 'p2', 'p3'],
                              		      sweep_time=[5,10,15,20],
                              		      project_name='sweep_hmm',
-                             		      model_name='gravel_sweep_100kb_uniform_scoeff')
+                             		      model_name='gravel_sweep_400kb_uniform_scoeff')
 
--- popgen_scripts/generate_slim_input_neutral.py
+++ popgen_scripts/generate_slim_input_neutral.py
@@ -1,6 +1,11 @@
 from popgen_utils.haplotype_simulation import slim
-slim.slim_definition_to_input_neutral('gravel_model_neutral_100kb',
+# slim.slim_definition_to_input_neutral('gravel_model_neutral_100kb',
+#                              		      project_name='sweep_hmm',
+#                              		      model_name='gravel_neutral_100kb',
+#                              		      sims=1000)
+
+slim.slim_definition_to_input_neutral('gravel_model_neutral_400kb',
                              		      project_name='sweep_hmm',
-                             		      model_name='gravel_neutral_100kb',
+                             		      model_name='gravel_neutral_400kb',
                              		      sims=1000)
 
