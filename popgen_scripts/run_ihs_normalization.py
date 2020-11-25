de592c8abf5ff6d61e685301dc5721a37755bc7e
Lauren Sugden <laurensugden@Eliza.local>
2020-11-25 15:44:58 -0500

    input 2 paths

--- popgen_scripts/run_ihs_normalization.py
+++ popgen_scripts/run_ihs_normalization.py
@@ -1,4 +1,6 @@
 from popgen_utils.snp_statistics import selscan
 
 selscan.normalize_ihs(project_name='sweep_hmm',
-             model_name='gravel_sweep_400kb_uniform_scoeff')
+             model_name_neutral='gravel_neutral_400kb',
+             model_name_sweep='gravel_sweep_400kb_uniform_scoeff')
+
