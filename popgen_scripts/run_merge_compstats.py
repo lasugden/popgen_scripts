42254f24b415d8a04fda56b4c04dbe84f6770bbc
Lauren Sugden <laurensugden@Eliza.local>
2020-11-25 12:59:50 -0500

    script to run merge_compstats

---
+++ popgen_scripts/run_merge_compstats.py
@@ -0,0 +1,7 @@
+from popgen_utils.misc import merge_compstats_for_swifr
+
+merge_compstats_for_swifr.merge_compstats(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',type='sweep')
+
+merge_compstats_for_swifr.merge_compstats(project_name='sweep_hmm',
+             model_name='gravel_neutral_400kb',type='neutral')
