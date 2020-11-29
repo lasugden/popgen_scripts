de936d50200463dd9a14258ac3c5a9a664c0331b
Lauren Sugden <laurensugden@Eliza.local>
2020-11-29 16:26:20 -0500

    extract linked right

---
+++ popgen_scripts/run_extract_allstats_linkedright.py
@@ -0,0 +1,13 @@
+from popgen_utils.misc import merge_compstats_for_swifr
+
+merge_compstats_for_swifr.extract_compstats_region(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',pop_of_interest='p1',
+             sweep_pos_1=200001, sweep_pos_2=300000, additional_name_text='linked_right')
+
+merge_compstats_for_swifr.extract_compstats_region(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',pop_of_interest='p2',
+             sweep_pos_1=200001, sweep_pos_2=300000, additional_name_text='linked_right')
+
+merge_compstats_for_swifr.extract_compstats_region(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',pop_of_interest='p3',
+             sweep_pos_1=200001, sweep_pos_2=300000, additional_name_text='linked_right')
