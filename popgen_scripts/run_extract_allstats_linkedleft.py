ed2cf76c3c3203f45dd1d8a9b51ccf98b32b3d13
Lauren Sugden <laurensugden@Eliza.local>
2020-11-29 16:13:44 -0500

    extract linked left sites (from 100000 to 200000)

---
+++ popgen_scripts/run_extract_allstats_linkedleft.py
@@ -0,0 +1,13 @@
+from popgen_utils.misc import merge_compstats_for_swifr
+
+merge_compstats_for_swifr.extract_compstats_region(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',pop_of_interest='p1',
+             sweep_pos_1=100000, sweep_pos_2=199999 additional_name_text='linked_left')
+
+merge_compstats_for_swifr.extract_compstats_region(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',pop_of_interest='p2',
+             sweep_pos_1=100000, sweep_pos_2=199999 additional_name_text='linked_left')
+
+merge_compstats_for_swifr.extract_compstats_region(project_name='sweep_hmm',
+             model_name='gravel_sweep_400kb_uniform_scoeff',pop_of_interest='p3',
+             sweep_pos_1=100000, sweep_pos_2=199999 additional_name_text='linked_left')
