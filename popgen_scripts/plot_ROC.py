589f5425417469c48aa2021c78c675b8211e9de7
Lauren Sugden <laurensugden@Eliza.local>
2020-12-15 11:46:02 -0600

    memory debugging

--- popgen_scripts/plot_ROC.py
+++ popgen_scripts/plot_ROC.py
@@ -3,12 +3,14 @@ from popgen_utils.performance_analysis import ROC_analysis
 ROC_analysis.make_ROC_curves(project_name='sweep_hmm', swifr_out_path='swifr_testing/p1',
 	swifr_train_path='gravel_400kb_p1_swifr', model_name_neutral='gravel_neutral_400kb',
 	model_name_sweep='gravel_sweep_400kb_uniform_scoeff', sweep_pos=200000, pop_of_interest='p1',
-	mode_neg='neutral', mode_pos='sweep')
+	mode_neg='neutral', mode_pos='linked')
 
 ROC_analysis.make_ROC_curves(project_name='sweep_hmm', swifr_out_path='swifr_testing/p1',
 	swifr_train_path='gravel_400kb_p1_swifr', model_name_neutral='gravel_neutral_400kb',
 	model_name_sweep='gravel_sweep_400kb_uniform_scoeff', sweep_pos=200000, pop_of_interest='p1',
-	mode_neg='neutral', mode_pos='linked')
+	mode_neg='neutral', mode_pos='sweep')
+
+
 
 ROC_analysis.make_ROC_curves(project_name='sweep_hmm', swifr_out_path='swifr_testing/p1',
 	swifr_train_path='gravel_400kb_p1_swifr', model_name_neutral='gravel_neutral_400kb',
